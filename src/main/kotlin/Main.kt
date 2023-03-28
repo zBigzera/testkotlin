970627b026d52f480505d34ed0081bb26e246d4e
Otavio Bigogno <otaviobigogno37@gmail.com>
2023-03-28 20:33:17 -0300

    Testes e testes

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,29 @@
+fun main() {
+
+    elseiflinha()
+    helloWorld("Otávio")
+    ifelif()
+}
+fun ifelif() {
+    var time = 13
+    if (time < 12) {
+        println("Bom Dia.")
+    } else if (time < 18) {
+        println("Boa Tarde.")
+    } else {
+        println("Boa Noite.")
+    }
+}
+fun helloWorld(name: String) {
+    val message = "Olá, $name!"
+    println(message)
+}
+fun elseiflinha(){
+    var time2 = 20
+    var sim = if (time2 < 18) {
+        "Ohayo."
+    } else {
+        "Boa Noite."
+    }
+    println(sim)
+}
